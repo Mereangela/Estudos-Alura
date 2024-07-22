40a8b3f7d7d8659b2fc32faacd9fd537b06d03f7
Mere Angela <mereangeladasilva@gmail.com>
2024-07-22 17:13:33 -0300

    Alteração

--- JavaScript/Projeto_4/app/reconhecimentoVoz.js
+++ JavaScript/Projeto_4/app/reconhecimentoVoz.js
@@ -1,13 +1,14 @@
 const elementoChute = document.getElementById('chute')
 
-window.SpeechRecognition = window.SpeechRecognition ||
-    webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
+const numero = document.getElementById('box');
+
 recognition.lang = 'pt-Br'
 recognition.start()
 
-recognition.addEventListener('result', onSpeak)
+recognition.addEventListener('result', onSpeak);
 
 function onSpeak(e) {
     chute = e.results[0][0].transcript
--- JavaScript/Projeto_4/app/validacao.js
+++ JavaScript/Projeto_4/app/validacao.js
@@ -22,10 +22,10 @@ function verificaSeChutePossuiValorValido(chute) {
         <button id="jogar-novamente" class="btn-jogar">Tente Novamente</button>
         `
     } else if (numero > numeroSecreto) {
-        elementoChute.innerHTML += `< div > O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        elementoChute.innerHTML += `<div> O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `
     } else {
-        elementoChute.innerHTML += `< div > O número secreto é maior  <i class="fa-solid fa-up-long"></i></div>
+        elementoChute.innerHTML += `<div> O número secreto é maior  <i class="fa-solid fa-up-long"></i></div>
         `
     }
 }
